ac14d8dcbcaf28b8e7ee5f65a8becf34e8ca4f10
Koray Vatansever <koray-vatansever@hotmail.com>
2025-04-19 20:57:36 +0300

    updt

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,26 +10,23 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category> ();
+        ICategoryDal _categorydal;
 
-        public List<Category> GetAll()
+        public CategoryManager(ICategoryDal categorydal)
         {
-            return repo.List();
+            _categorydal = categorydal;
         }
 
-        public void CategoryAdd(Category p)
+        public void CategoryAdd(Category category)
         {
-            if(p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length > 51)
-            {
-                //Hata mesajı
-            }
+            _categorydal.Insert(category);
+        }
 
-            else
-            {
-                repo.Insert(p);
-            }
+        public List<Category> GetList()
+        {
+            return _categorydal.List();
         }
     }
 }
