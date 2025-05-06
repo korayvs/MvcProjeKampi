e7f83cba971cb6104317b45dd382739ca1b5d016
Koray Vatansever <koray-vatansever@hotmail.com>
2025-05-06 19:58:40 +0300

    64 upd

---
+++ MvcProjeKampi/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager mm = new MessageManager(new EfMessageDal());
+
+        public ActionResult Inbox()
+        {
+            var listin = mm.GetListInbox();
+            return View(listin);
+        }
+
+        public ActionResult Sendbox()
+        {
+            var listsend = mm.GetListSendbox();
+            return View(listsend);
+        }
+
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            return View();
+        }
+    }
+}
